5662036d2db045f2872189f0192f04f8fb8591b3
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2007-02-23 20:36:37 +0000

    Moved get_default_room and get_default_area into their input files

--- trunk/schoorbs-includes/input/area.input.php
+++ trunk/schoorbs-includes/input/area.input.php
@@ -6,6 +6,23 @@
  * @package Schoorbs
  * @subpackage Input
  */
+
+/**
+ * Return a default area; used if no area is already known. This returns the
+ * lowest area ID in the database (no guaranty there is an area 1).
+ * This could be changed to implement something like per-user defaults.
+ * 
+ * @author Uwe L. Korn <uwelk@xhochy.org>
+ * @return int the ID of the default Area
+ */ 
+function get_default_area()
+{
+	global $tbl_area;
+	
+	$area = sql_query1("SELECT id FROM $tbl_area ORDER BY area_name LIMIT 1");
+	
+	return ($area < 0 ? 0 : $area);
+}
  
 /**
  * Catches the Area out of the REQUEST-Array and the defaults
--- trunk/schoorbs-includes/input/room.input.php
+++ trunk/schoorbs-includes/input/room.input.php
@@ -8,6 +8,25 @@
  */
  
 /**
+ * Return a default room given a valid area; used if no room is already known.
+ * This returns the first room in alphbetic order in the database.
+ * This could be changed to implement something like per-user defaults.
+ * 
+ * @author Uwe L. Korn <uwelk@xhochy.org>, jberanek
+ * @param int $nArea
+ * @return int the ID of the default room
+ */ 
+function get_default_room($nArea)
+{
+	global $tbl_room;
+	
+	$room = sql_query1('SELECT id FROM '.$tbl_room.' WHERE area_id = \''
+		.sql_escape_arg($nArea).'\' ORDER BY room_name LIMIT 1');
+
+	return ($room < 0 ? 0 : $room);
+} 
+ 
+/**
  * Catches the Room out of the REQUEST-Array and the defaults
  * 
  * @author Uwe L. Korn <uwelk@xhochy.org>
