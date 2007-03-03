d226961f19c9eaacb260e9a91fb5c0cb80983a1e
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2007-03-03 12:44:05 +0000

    Exported pview input parsing into a seperate file

--- trunk/schoorbs-includes/input.functions.php
+++ trunk/schoorbs-includes/input.functions.php
@@ -12,4 +12,4 @@
 require_once 'input/daymonthyear.input.php';
 require_once 'input/area.input.php';
 require_once 'input/room.input.php';
-?>
+require_once 'input/pview.input.php';
---
+++ trunk/schoorbs-includes/input/pview.input.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Input-Plugin 'PView'
+ * 
+ * @author Uwe L. Korn <uwelk@xhochy.org>
+ * @package Schoorbs
+ * @subpackage Input
+ */
+ 
+/**
+ * Check if the page should be shown for printing
+ * 
+ * @author Uwe L. Korn <uwelk@xhochy.org>
+ * @return int
+ */ 
+function input_PView()
+{
+	if (!isset($_REQUEST['pview'])) 
+		return 0;
+	else
+	    return intval($_REQUEST['pview']);
+}
