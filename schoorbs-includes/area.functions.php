dfa33b565a217f4b55d8b7b759f1ed8c92156374
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2007-03-08 15:27:02 +0000

    Created new function getAreas()

--- schoorbs-includes/area.functions.php
+++ schoorbs-includes/area.functions.php
@@ -16,6 +16,8 @@
  */
 function areaGetName($nAreaID)
 {
+	global $tbl_area;
+
 	$res = sql_query("SELECT area_name FROM $tbl_area WEHRE id = ".sql_escape_arg($nAreaID));
 	if (! $res) fatal_error(0, sql_error());
 	if (sql_count($res) == 1)
@@ -25,4 +27,23 @@ function areaGetName($nAreaID)
 	}
 	sql_free($res);
 	return $area_name;
+}
+
+/**
+ * Get an array of all known areas
+ * 
+ * @return array[][] array(array(id, area_name))
+ */
+function getAreas()
+{
+	global $tbl_area;
+	
+	$sQuery = "SELECT id, area_name FROM $tbl_area ORDER BY area_name";
+   	$res = sql_query($sQuery);
+   	$rows = array();
+   	if ($res) for ($i = 0; ($row = sql_row($res, $i)); $i++)
+   	{
+   		$rows[] = array('id' => $row[0], 'area_name' => $row[1]);
+    }
+    return $rows;
 }
