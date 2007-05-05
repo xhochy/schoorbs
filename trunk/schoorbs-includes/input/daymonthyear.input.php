11bb907ff03f5a0e18e385dab73aa5f8bf01dd37
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2007-05-05 19:04:15 +0000

    Some code beautifing

--- trunk/schoorbs-includes/input/daymonthyear.input.php
+++ trunk/schoorbs-includes/input/daymonthyear.input.php
@@ -17,38 +17,36 @@
 function input_DayMonthYear()
 {
 	/** day **/	
-	if(!isset($_REQUEST['day']))
+	if (!isset($_REQUEST['day'])) {
 		$day   = date("d");
-	else
-	{
+	} else {
 	    $day = intval(unslashes($_REQUEST['day']));
 	    if($day < 1) $day = 1;
 	    if($day > 31) $day = 31;
 	}	
 	
 	/** month **/
-	if(!isset($_REQUEST['month']))
+	if (!isset($_REQUEST['month'])) {
 		$month = date('m');
-	else
-	{
+	} else {
 		$month = intval(unslashes($_REQUEST['month']));
 	    if($month < 1) $month = 1;
 	    if($month > 12) $month = 12;
 	}
 	
 	/** year **/
-	if(!isset($_REQUEST['year']))
+	if (!isset($_REQUEST['year'])) {
 		$year = date("Y");
-	else
-	{
+	} else {
 		$year = intval(unslashes($_REQUEST['year']));
-	    if($year < 1970) $year = 1970; //there should't be a installation of Schoorbs that go back to 1970
+	    if($year < 1970) $year = 1970; //there should't be a installation of Schoorbs that goes back to 1970
 	    if($year > 2100) $year = 2100; //Will somebody use Schoorbs in 2100?
 	}
 
     # Make the date valid if day is more then number of days in month
-	while (!checkdate($month, $day, $year))
+	while (!checkdate($month, $day, $year)) {
 		$day--;
+	}
 	
 	return array($day, $month, $year);
 }
