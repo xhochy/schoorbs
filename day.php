e4d6c57bbd85ae5c0b2856710f4c90decb504fcc
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2007-06-07 15:57:41 +0000

    Made code of day.php a bit nicer

--- day.php
+++ day.php
@@ -40,7 +40,7 @@ list($td, $tm, $ty) = getTomorrow($day, $month, $year);
 print_header($day, $month, $year, $area);
 
 $format = "Gi";
-if( $enable_periods ) {
+if ($enable_periods) {
 	$format = "i";
 	$resolution = 60;
 	$morningstarts = 12;
@@ -54,16 +54,18 @@ if($pview != 1)
     # need to show either a select box or a normal html list,
     # depending on the settings in config.inc.php
     if ($area_list_format == "select")
-	    $smarty->assign('area_select_list',make_area_select_html('day.php', $area, $year, $month, $day));
+	    $smarty->assign('area_select_list', make_area_select_html('day.php', $area, $year, $month, $day));
     else
     	$smarty->assign('areas', getAreas()); // show the standard html list
    
-    $smarty->assign('area',$area);
-    $smarty->assign('day',$day);
-    $smarty->assign('year',$year);
-    $smarty->assign('month',$month);
-    $smarty->assign('dwm','day.php');
-    $smarty->assign('area_list_format',$area_list_format);
+    $smarty->assign(array(
+    	'area' => $area,
+    	'day' => $day,
+    	'year' => $year,
+    	'month' => $month,
+    	'dwm' => 'day.php',
+    	'area_list_format' => $area_list_format
+    ));
     $smarty->display('area_list.tpl');
     
     #Draw the three month calendars
@@ -115,8 +117,7 @@ for ($i = 0; ($row = sql_row($res, $i)); $i++) {
 	# start or end at a recognized time still appear.
 	$start_t = max(round_t_down($row[1], $resolution, $am7), $am7);
 	$end_t = min(round_t_up($row[2], $resolution, $am7) - $resolution, $pm7);
-	for ($t = $start_t; $t <= $end_t; $t += $resolution)
-	{
+	for ($t = $start_t; $t <= $end_t; $t += $resolution) {
 		$today[$row[0]][date($format,$t)]["id"]    = $row[4];
 		$today[$row[0]][date($format,$t)]["color"] = $row[5];
 		$today[$row[0]][date($format,$t)]["data"]  = "";
@@ -126,13 +127,10 @@ for ($i = 0; ($row = sql_row($res, $i)); $i++) {
 
 	# Show the name of the booker in the first segment that the booking
 	# happens in, or at the start of the day if it started before today.
-	if ($row[1] < $am7)
-	{
+	if ($row[1] < $am7) {
 		$today[$row[0]][date($format,$am7)]["data"] = $row[3];
 		$today[$row[0]][date($format,$am7)]["long_descr"] = $row[6];
-	}
-	else
-	{
+	} else {
 		$today[$row[0]][date($format,$start_t)]["data"] = $row[3];
 		$today[$row[0]][date($format,$start_t)]["long_descr"] = $row[6];
 	}
@@ -144,60 +142,45 @@ for ($i = 0; ($row = sql_row($res, $i)); $i++) {
 
 $sQuery = "SELECT room_name, capacity, id, description FROM $tbl_room WHERE area_id = "
 	.sql_escape_arg($area)." ORDER BY 1";
-
 $res = sql_query($sQuery);
 
 # It might be that there are no rooms defined for this area.
 # If there are none then show an error and dont bother doing anything
 # else
 if (! $res) fatal_error(0, sql_error());
-if (sql_count($res) == 0)
-{
+if (sql_count($res) == 0) {
 	echo "<h1>".get_vocab("no_rooms_for_area")."</h1>";
 	sql_free($res);
-}
-else
-{
-	$smarty->assign('am7', utf8_strftime("%A %d %B %Y", $am7));
-	$smarty->assign('pview',$pview);
-	$smarty->assign('yy',$yy);
-	$smarty->assign('ym',$ym);
-	$smarty->assign('yd',$yd);
-	$smarty->assign('ty',$ty);
-	$smarty->assign('tm',$tm);
-	$smarty->assign('td',$td);
-	$smarty->assign('year',$year);
-	$smarty->assign('day',$day);
-	$smarty->assign('month',$month);
-	if($javascript_cursor)
-		$smarty->assign('javascript_cursor','true');
-	else
-		$smarty->assign('javascript_cursor','false');
-	if($show_plus_link)
-		$smarty->assign('show_plus_link','true');
-	else
-		$smarty->assign('show_plus_link','false');
-	if($times_right_side)
-		$smarty->assign('times_right_side','true');
-	else
-		$smarty->assign('times_right_side','false');
-	if($enable_periods)
-		$smarty->assign('enable_periods','true');
-	else
-		$smarty->assign('enable_periods','false');
-	$smarty->assign('highlight_method',$highlight_method);
-	$smarty->assign('area',$area);
-	$smarty->assign('period_title',$enable_periods ? get_vocab("period") : get_vocab("time"));
-	
+} else {
 	$room_column_width = (int)(95 / sql_count($res));
+	
+	$smarty->assign(array(
+		'am7' => utf8_strftime("%A %d %B %Y", $am7),
+		'pview' => $pview,
+		'yy' => $yy, 'ym' => $ym, 'yd' => $yd,
+		'ty' => $ty, 'tm' => $tm, 'td' => $td,
+		'year' => $year,
+		'day' => $day,
+		'month' => $month,
+		'javascript_cursor' => ($javascript_cursor ? 'true' : 'false'),
+		'show_plus_link' => ($show_plus_link ? 'true' : 'false'),
+		'times_right_side' => ($times_right_side ? 'true' : 'false'),
+		'enable_periods' => ($enable_periods ? 'true' : 'false'),
+		'highlight_method' => $highlight_method,
+		'area' => $area,
+		'period_title' => ($enable_periods ? get_vocab("period") : get_vocab("time")),
+		'room_column_width' => $room_column_width
+	));
+	
+	
 	$aRooms = array();
 	for ($i = 0; ($row = sql_row($res, $i)); $i++)
 	{
 	    $rooms[] = $row[2];
-	    $aRooms[] = array('title' => $row[0], 'capacity' => $row[1], 'id' => $row[2], 'description' => $row[3]);
+	    $aRooms[] = array('title' => $row[0], 'capacity' => $row[1], 
+	    	'id' => $row[2], 'description' => $row[3]);
 	}
-	$smarty->assign('room_column_width',$room_column_width);
-	$smarty->assign('rooms',$aRooms);
+	$smarty->assign('rooms', $aRooms);
 	
 	# URL for highlighting a time. Don't use REQUEST_URI or you will get
 	# the timetohighlight parameter duplicated each time you click.
@@ -218,20 +201,17 @@ else
 		$t = mktime($morningstarts, $morningstarts_minutes, 0, $month, $day+$j, $year);
 		$t <= mktime($eveningends, $eveningends_minutes, 0, $month, $day+$j, $year);
 		$t += $resolution, $row_class = ($row_class == "even_row")?"odd_row":"even_row"
-	)
-	{
+	) {
 		# convert timestamps to HHMM format without leading zeros
 		$time_t = date($format, $t);
 		
 		$cols = array();
 		// Loop through the list of rooms we have for this area
-		foreach($rooms as $key=>$room)
-		{
+		foreach($rooms as $key => $room) {
 			// Array used to temporarly store the vars that will be sent to Smarty
 			$aLoop = array();
 			
-			if(isset($today[$room][$time_t]['id']))
-			{
+			if(isset($today[$room][$time_t]['id'])) {
 				$aLoop['id'] = $today[$room][$time_t]["id"];
 				$color = $today[$room][$time_t]["color"];
 				$aLoop['descr'] = htmlspecialchars($today[$room][$time_t]["data"]);
@@ -255,22 +235,20 @@ else
 				$hour = date("H",$t);
 				$minute  = date("i",$t);
 				
-				if($pview != 1) 
-				{
-					if( $enable_periods ) 
-					{
+				if ($pview != 1) {
+					if ($enable_periods) {
 						$time_t_stripped = preg_replace( "/^0/", "", $time_t ); 
 						$aLoop['period_param'] = "&amp;period=$time_t_stripped";
-					}
-					else
+					} else {
 						$aLoop['period_param'] = "&amp;hour=$hour&amp;minute=$minute";
+					}
 				}
 			}
 			$aLoop['room'] = $room;
 			$cols[] = $aLoop;
 		}
       	
-      	$time = array('time' => $time_t, 'cols' => $cols);
+	      	$time = array('time' => $time_t, 'cols' => $cols);
 		if($enable_periods)
 			$time['title'] = $periods[preg_replace( "/^0/", "", $time_t)];
 		else
@@ -278,10 +256,9 @@ else
 		$times[] = $time;
 	}
 	
-	$smarty->assign('times',$times);
+	$smarty->assign('times', $times);
 	$smarty->display('day.tpl');
-	
-    show_colour_key();
+	show_colour_key();
 }
 
 /** The footer of the HTML Page */
