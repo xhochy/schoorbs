c1855807fd4a2fa8000aab4cd5501a5bf87533d0
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2007-07-28 19:01:27 +0000

    Added template for the REST-API

---
+++ spinoffs/schoorbs-js-rest-client-lib/trunk/schoorbsREST.js
@@ -0,0 +1,6 @@
+var schoorbsREST = {};
+
+// When defining an endpoint, use the URL where we can find your Schoorbs installation,
+// do not append /REST/ to it. The last slash should be omitted, but if it is added, it 
+// just makes ugly URLs but should work without any problems.
+schoorbsREST.endPoint = "http://localhost/schoorbs";
