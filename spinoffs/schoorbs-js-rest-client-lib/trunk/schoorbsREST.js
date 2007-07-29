e74147f27321f02b04404685b1877cae287a4d18
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2007-07-29 14:17:31 +0000

    Added tests

--- spinoffs/schoorbs-js-rest-client-lib/trunk/schoorbsREST.js
+++ spinoffs/schoorbs-js-rest-client-lib/trunk/schoorbsREST.js
@@ -4,3 +4,29 @@ var schoorbsREST = {};
 // do not append /REST/ to it. The last slash should be omitted, but if it is added, it 
 // just makes ugly URLs but should work without any problems.
 schoorbsREST.endPoint = "http://localhost/schoorbs";
+
+// The username used to authenticate 
+schoorbsREST.user = "";
+// The password used to authenticate
+schoorbsREST.passwd = "";
+
+schoorbsREST.getRoomID = function (roomName) {
+	var req = new XMLHttpRequest();
+	req.open('GET', schoorbsREST.endPoint + '/REST/getRoomID?name=' + escape(roomName), false, 
+        schoorbsREST.user, schoorbsREST.passwd);
+    // it's not sure, that Schoorbs returns the content with a MimeType-Header
+    // (depends on the server on which Schoorbs is running) 
+    req.overrideMimeType('text/xml');
+	req.send(null);
+	if(req.status != 200) {
+        alert('Couldn\'t get RoomID!');
+		return false;
+	}
+	var xml = req.responseXML;
+    var result = xml.getElementsByTagName('room_id');
+    if (result.length > 0) {
+        return result[0];
+    } else {
+        return false;
+    }
+}
