0be340250e03cca2c6cb4ce6dc026eb6ee64666f
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2007-10-06 15:33:03 +0000

    Added makeBooking

--- spinoffs/schoorbs-js-rest-client-lib/trunk/schoorbsREST.js
+++ spinoffs/schoorbs-js-rest-client-lib/trunk/schoorbsREST.js
@@ -66,10 +66,39 @@ schoorbsREST.checkFree = function (saal, period_id, days) {
     req.overrideMimeType('text/xml');
 	req.send(null);
 	if (req.status != 200) {
-		return alert('Konnte Stunde nicht überprüfen');
+		alert('Konnte Stunde nicht überprüfen');
+        return false;
 	}
     var xml = req.responseXML;
     var result = xml.getElementsByTagName('free');
+    if (result.length > 0) {
+        return result[0].textContent == 'true';
+    } else {
+        return false;
+    }
+}
+
+schoorbsREST.makeBooking = function (saal, period_id, days, entry_name) {
+	var req = new XMLHttpRequest();
+	var req_url = schoorbsREST.endPoint + '/REST/makeBooking?period=' + period_id + "&room=" + saal;
+	req_url += "&type=B&name=" + escape(entry_name) + "&description=" + escape(entry_name);
+	for (var i = 0; i < days.length; i++) {
+		req_url += "&day[" + i.toString() + "]=" + days[i].getUTCDate().toString();
+		req_url += "&month[" + i.toString() + "]=" + (days[i].getUTCMonth() + 1).toString();
+		req_url += "&year[" + i.toString() + "]=" + days[i].getUTCFullYear().toString();
+	}
+	
+	req.open('GET', req_url, false);
+	// it's not sure, that Schoorbs returns the content with a MimeType-Header
+    // (depends on the server on which Schoorbs is running) 
+    req.overrideMimeType('text/xml');
+	req.send(null);
+	if (req.status != 200) {
+		alert('Konnte Stunde nicht reservieren');
+        return false;
+	}
+    var xml = req.responseXML;
+    var result = xml.getElementsByTagName('make_booking');
     if (result.length > 0) {
         return result[0].textContent == 'true';
     } else {
