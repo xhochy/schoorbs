05637f31e491980fc4c3f5ce7cae129ef6592ca8
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2007-10-06 14:03:05 +0000

    Added checkFree

--- spinoffs/schoorbs-js-rest-client-lib/trunk/schoorbsREST.js
+++ spinoffs/schoorbs-js-rest-client-lib/trunk/schoorbsREST.js
@@ -51,6 +51,32 @@ schoorbsREST.getPeriodID = function (periodName) {
     }
 }
 
+schoorbsREST.checkFree = function (saal, period_id, days) {
+	var req = new XMLHttpRequest();
+	var req_url = schoorbsREST.endPoint + '/REST/checkFree?period=' + period_id + "&room=" + saal;
+	for (var i = 0; i < days.length; i++) {
+		req_url += "&day[" + i.toString() + "]=" + days[i].getUTCDate().toString();
+		req_url += "&month[" + i.toString() + "]=" + (days[i].getUTCMonth() + 1).toString();
+		req_url += "&year[" + i.toString() + "]=" + days[i].getUTCFullYear().toString();
+	}
+	
+	req.open('GET', req_url, false);
+	// it's not sure, that Schoorbs returns the content with a MimeType-Header
+    // (depends on the server on which Schoorbs is running) 
+    req.overrideMimeType('text/xml');
+	req.send(null);
+	if (req.status != 200) {
+		return alert('Konnte Stunde nicht überprüfen');
+	}
+    var xml = req.responseXML;
+    var result = xml.getElementsByTagName('free');
+    if (result.length > 0) {
+        return result[0] == 'true';
+    } else {
+        return false;
+    }
+}
+
 schoorbsREST.login = function() {
     var req = new XMLHttpRequest();
 	req.open('GET', schoorbsREST.endPoint + '/REST/login/', false); 
