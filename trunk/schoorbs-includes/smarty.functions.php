20aa4cb55643c5c384d08b5f19d062927e030b4b
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2007-12-23 20:44:16 +0000

    Moved smarty-templates and our own smarty-plugins to new places

--- trunk/schoorbs-includes/smarty.functions.php
+++ trunk/schoorbs-includes/smarty.functions.php
@@ -10,12 +10,16 @@
 
 
 // put full path to Smarty.class.php
+require_once dirname(__FILE__).'/smarty-plugins/function.genDateSelector.php';
+require_once dirname(__FILE__).'/smarty-plugins/function.get_vocab.php';
 require_once dirname(__FILE__).'/Smarty/libs/Smarty.class.php';
 
 // Init Smarty
 $smarty = new Smarty();
 
-$smarty->template_dir = dirname(__FILE__).'/Smarty/templates';
+$smarty->template_dir = realpath(dirname(__FILE__).'/../schoorbs-misc/templates');
 $smarty->compile_dir = dirname(__FILE__).'/Smarty/templates_c';
 $smarty->cache_dir = dirname(__FILE__).'/Smarty/cache';
 $smarty->config_dir = dirname(__FILE__).'/Smarty/configs';
+$smarty->register_function('get_vocab', smarty_function_get_vocab);
+$smarty->register_function('genDateSelector', smarty_function_genDateSelector);
