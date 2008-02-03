3651336cdd358f5530a4e58a7dc66e214e711cc8
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-02-03 17:31:10 +0000

    Use realpath on Smarty directories for support on Debian

--- schoorbs-includes/smarty.functions.php
+++ schoorbs-includes/smarty.functions.php
@@ -19,9 +19,9 @@ require_once dirname(__FILE__).'/Smarty/libs/Smarty.class.php';
 /** Init Smarty */
 $smarty = new Smarty();
 
-$smarty->template_dir = realpath(dirname(__FILE__).'/../schoorbs-misc/templates');
-$smarty->compile_dir = dirname(__FILE__).'/Smarty/templates_c';
-$smarty->cache_dir = dirname(__FILE__).'/Smarty/cache';
-$smarty->config_dir = dirname(__FILE__).'/Smarty/configs';
+$smarty->template_dir = realpath(dirname(__FILE__).'/../schoorbs-misc/templates/');
+$smarty->compile_dir = realpath(dirname(__FILE__).'/Smarty/templates_c/');
+$smarty->cache_dir = realpath(dirname(__FILE__).'/Smarty/cache/');
+$smarty->config_dir = realpath(dirname(__FILE__).'/Smarty/configs/');
 $smarty->register_function('get_vocab', smarty_function_get_vocab);
 $smarty->register_function('genDateSelector', smarty_function_genDateSelector);
