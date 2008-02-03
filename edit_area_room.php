6a63bb7d216ebf3687e1c1867cb96358b5079ada
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-02-03 14:45:28 +0000

    Code cleanup in edit_area_room.php

--- edit_area_room.php
+++ edit_area_room.php
@@ -6,60 +6,63 @@
  * @package Schoorbs
  * @subpackage Admin
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
- * @todo add Mail functions which had been available in MRBS, but were removed due to license Issues
  */
 
+/** The Configuration file */
 require_once 'config.inc.php';
+/** The general 'things' when viewing Schoorbs on the web */
 require_once 'schoorbs-includes/global.web.php';
+/** The general functions */ 
 require_once 'schoorbs-includes/global.functions.php';
+/** The database wrapper */
 require_once "schoorbs-includes/database/$dbsys.php";
+/** The authetication wrappers */
 require_once 'schoorbs-includes/authentication/schoorbs_auth.php';
 
-## Input ##
+## Var Init ##
 
-#If we dont know the right date then make it up
+/** day, month, year */
 list($day, $month, $year) = input_DayMonthYear();
 
-if (isset($_REQUEST['room'])) {
-	$room = input_Room();
-}
-elseif (isset($_REQUEST['area'])) {
-	$area = input_Area();
-}
+if (isset($_REQUEST['room'])) $room = input_Room();
+elseif (isset($_REQUEST['area'])) $area = input_Area();
 
 ## Main ##
 
-if (!getAuthorised(2)) {
-	showAccessDenied();
-}
+if (!getAuthorised(2)) showAccessDenied();
 
 // Done changing area or room information?
 if (isset($_REQUEST['change_done'])) {
-	if (!empty($room)) { // Get the area the room is in
+	if (isset($room)) { // Get the area the room is in
 		$area = sql_query1("SELECT area_id from $tbl_room where id = ".sql_escape_arg($room));
 	}
 	header("Location: admin.php?day=$day&month=$month&year=$year&area=$area");
 	exit();
 } elseif (isset($_REQUEST['change_room'])) {
 	$sRoomName = unslashes($_REQUEST['room_name']);
-        $sDescription = unslashes($_REQUEST['description']);
-	$nCapacity = intval($_REQUEST['capacity']);
+    $sDescription = input_Description();
+	$nCapacity = input_Capacity();
  
-	$sQuery = "UPDATE $tbl_room SET room_name = '".sql_escape_arg($sRoomName).'\', '
-		.'description = \''.sql_escape_arg($sDescription).'\', '
-		."capacity = $nCapacity WHERE id = ".sql_escape_arg($room);
+	$sQuery = sprintf(
+		'UPDATE %s SET room_name = \'%s\', description = \'%s\', capacity = %d '
+		.'WHERE id = %d', 
+		$tbl_room, sql_escape_arg($sRoomName), sql_escape_arg($sDescription), 
+		$nCapacity, $room
+	);
 	sql_query($sQuery);
 } elseif (isset($_REQUEST['change_area'])) {
 	$sAreaName = unslashes($_REQUEST['area_name']);
 
-	$sQuery = "UPDATE $tbl_area SET area_name = '".sql_escape_arg($sAreaName)
-		."' WHERE id = ".sql_escape_arg($area);
+	$sQuery = sprintf(
+		'UPDATE %S SET area_name = \'%s\' WHERE id = %d', 
+		$tbl_area, sql_escape_arg($sAreaName), sql_escape_arg($area)
+	);
 	sql_query($sQuery);
 }
 
 print_header();
 
-if(!empty($room)) {
+if(isset($room)) {
 	
 	$res = sql_query("SELECT * FROM $tbl_room WHERE id = ".sql_escape_arg($room));
 	if (!$res) {
@@ -71,9 +74,9 @@ if(!empty($room)) {
 
 	$smarty->assign('row', $row);
 	$smarty->assign('room', $room);
-} elseif(!empty($area)) {
+} elseif(isset($area)) {
 
-    	$res = sql_query("SELECT * FROM $tbl_area WHERE id = ".sql_escape_arg($area));
+    $res = sql_query("SELECT * FROM $tbl_area WHERE id = ".sql_escape_arg($area));
 	if (!$res) {
 		$sMessage = get_vocab("error_area").$area.get_vocab("not_found");
 		fatal_error(0, $sMessage);
