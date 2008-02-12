6473b4a51749fea2bd39c970e6aa97e67d1a2494
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-02-12 20:55:18 +0000

    Updated the input of rep_end_day, rep_end_month, rep_end_year. Let input_DayMonthYear support prefixes like prefix = 'aa_' => day -> 'aa_day'.

--- schoorbs-includes/input/daymonthyear.input.php
+++ schoorbs-includes/input/daymonthyear.input.php
@@ -12,33 +12,34 @@
  * Catches the Day,Month and Year out of the REQUEST-Array and the defaults
  * 
  * @author Uwe L. Korn <uwelk@xhochy.org>
+ * @param $sVarPrefix string Be able to handle e.g. rep_end_month too
  * @return array (day,month,year)
  */ 
-function input_DayMonthYear()
+function input_DayMonthYear($sVarPrefix = '')
 {
 	/** day **/	
-	if (!isset($_REQUEST['day'])) {
+	if (!isset($_REQUEST[$sVarPrefix.'day'])) {
 		$day   = date("d");
 	} else {
-	    $day = intval(unslashes($_REQUEST['day']));
+	    $day = intval(unslashes($_REQUEST[$sVarPrefix.'day']));
 	    if($day < 1) $day = 1;
 	    if($day > 31) $day = 31;
 	}	
 
 	/** month **/
-	if (!isset($_REQUEST['month'])) {
+	if (!isset($_REQUEST[$sVarPrefix.'month'])) {
 		$month = date('m');
 	} else {
-		$month = intval(unslashes($_REQUEST['month']));
+		$month = intval(unslashes($_REQUEST[$sVarPrefix.'month']));
 	    if($month < 1) $month = 1;
 	    if($month > 12) $month = 12;
 	}
 	
 	/** year **/
-	if (!isset($_REQUEST['year'])) {
+	if (!isset($_REQUEST[$sVarPrefix.'year'])) {
 		$year = date("Y");
 	} else {
-		$year = intval(unslashes($_REQUEST['year']));
+		$year = intval(unslashes($_REQUEST[$sVarPrefix.'year']));
 	    if($year < 1970) $year = 1970; //there should't be a installation of Schoorbs that goes back to 1970
 	    if($year > 2100) $year = 2100; //Will somebody use Schoorbs in 2100?
 	}
