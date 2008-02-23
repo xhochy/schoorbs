a9214491403d2e96820ae448a6d83aa9b90068e8
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-02-23 16:43:35 +0000

    Code cleanup in Auth/Config.

--- schoorbs-includes/authentication/auth_config.php
+++ schoorbs-includes/authentication/auth_config.php
@@ -7,12 +7,11 @@
  * Then for each user, add an entry formatted as:
  * $auth["user"]["username"] == "userpassword";
  * 
+ * @todo Make passwords md5/sha1 crypted
  * @author jberanek, JFL
  * @package Schoorbs/Auth/Config
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
  */ 
-
-//TODO: Make passwords md5/sha1 crypted
 
 /**
  * Checks if the specified username/password pair are valid
@@ -24,27 +23,17 @@
  */
 function authValidateUser($user, $pass)
 {
-   global $auth;
+	global $auth;
 
 	// Check if we do not have a username/password
-	if(!isset($user) || !isset($pass) || strlen($pass)==0)
-	{
-		return 0;
-	}
-
-	$bOk = 0;
-	if(isset($auth['user'][$user]))
-		if($auth['user'][$user] == $pass) $bOk++;
-		
-	if(isset($auth['user'][strtolower($user)]))
-		if($auth['user'][strtolower($user)] == $pass) $bOk++; 
-
-	if($bOk > 0)//or is $bOK == 1 more secure ?
-	{
-		return 1; // User validated
-	}
-
-	return 0;		// User unknown or password invalid
+	if(!isset($user) || !isset($pass) || strlen($pass) == 0) return false;
+	// Check if	the user exists
+	if(isset($auth['user'][$user]) && ($auth['user'][$user] == $pass)) return true;
+	// Check if the user exists in lowercase type
+	$user = strtolower($user);
+	if(isset($auth['user'][$user]) && ($auth['user'][$user] == $pass)) return true;
+	// User unknown or password invalid
+	return false;		
 }
 
 /* authGetUserLevel($user)
@@ -59,14 +48,11 @@ function authValidateUser($user, $pass)
 function authGetUserLevel($user, $lev1_admin)
 {
    // User not logged in, user level '0'
-   if(!isset($user))
-      return 0;
+   if(!isset($user)) return 0;
 
    // Check if the user is can modify
-   for($i = 0; isset($lev1_admin[$i]); $i++)
-   {
-      if(strcasecmp($user, $lev1_admin[$i]) == 0)
-	 return 2;
+   for ($i = 0; isset($lev1_admin[$i]); $i++) {
+      if(strcasecmp($user, $lev1_admin[$i]) == 0) return 2;
    }
 
    // Everybody else is access level '1'
