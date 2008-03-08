13332db433934a1ef1e8d9376ac824f1c2ed2559
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-03-08 01:39:49 +0000

    Added comments to del_entry.php.

--- trunk/del_entry.php
+++ trunk/del_entry.php
@@ -31,12 +31,18 @@ if (isset($_REQUEST['series'])) $series = intval($_REQUEST['series']);
 
 ## Main ##
 
+// Check only if the user is logged in, if he has enough rights to delete
+// will be checked later on in schoorbsDelEntry()
 if (getAuthorised(1) && ($info = mrbsGetEntryInfo($id))) {
+	// Get day, month and year of the booking which should be deleted
 	$day   = date('d', $info['start_time']);
 	$month = date('m', $info['start_time']);
 	$year  = date('Y', $info['start_time']);
 	$area  = mrbsGetRoomArea($info['room_id']);
 
+	// Do not delete entries which are already in progress or have happened in
+	// the past. This should prohibit abuse when using the room and resource
+	// booking system.
     if ($info['start_time'] <= time()) {
         /** @todo Translate this */
         fatal_error(true, 'Start time in Past, could not delete entry!');
@@ -46,8 +52,9 @@ if (getAuthorised(1) && ($info = mrbsGetEntryInfo($id))) {
 	$result = schoorbsDelEntry(getUserName(), $id, $series, 1);
 	sql_commit();
 	if ($result) {
-        // Log deletion of entry
+        // Log deletion of an entry
         schoorbsLogDeletedEntry($info);
+        // Return to the day where we deleted that entry
         header("Location: day.php?day=$day&month=$month&year=$year&area=$area");
 		exit();
 	}
