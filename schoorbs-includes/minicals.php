1f70aeb07a20bf42ca813c0703b830f042bef9ed
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-03-16 12:23:50 +0000

    Added some comments to the Minicals class.

--- schoorbs-includes/minicals.php
+++ schoorbs-includes/minicals.php
@@ -56,31 +56,25 @@ class Calendar
    
     function getDaysInMonth($month, $year)
     {
-        if ($month < 1 || $month > 12)
-        {
+        if ($month < 1 || $month > 12) {
             return 0;
         }
     
         $days = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
    
         $d = $days[$month - 1];
-   
-        if ($month == 2)
-        {
-            // Check for leap year
-            // Forget the 4000 rule, I doubt I'll be around then...
-        
-            if ($year%4 == 0)
-            {
-                if ($year%100 == 0)
-                {
-                    if ($year%400 == 0)
-                    {
+
+		// Check for leap year
+        // Forget the 4000 rule, I doubt I'll be around then...   
+        // If it's a leap year set the returned value to 29, else leave it on 
+        // 28.
+        if ($month == 2) {
+            if ($year%4 == 0) {
+                if ($year%100 == 0) {
+                    if ($year%400 == 0) {
                         $d = 29;
                     }
-                }
-                else
-                {
+                } else {
                     $d = 29;
                 }
             }
@@ -91,30 +85,26 @@ class Calendar
 
     function getHTML()
     {
-        global $weekstarts, $smarty;
-        global $day;
-        global $month;
+        global $weekstarts, $smarty, $day, $month;
 
         if (!isset($weekstarts)) $weekstarts = 0;
-        $s = "";
         
         $daysInMonth = $this->getDaysInMonth($this->month, $this->year);
 		// $prevYear is the current year unless the previous month is
 		// December then you need to decrement the year
-		if( $this->month - 1 > 0 )
-		{
+		if (($this->month - 1) > 0) {
 			$prevMonth = $this->month - 1;
 			$prevYear = $this->year;
-		}
-		else
-		{
+		} else {
 			$prevMonth = 12;
 			$prevYear = $this->year -1;
 		}
         $daysInPrevMonth = $this->getDaysInMonth($prevMonth, $prevYear);
+        // Get the time of this month, 1st, 12:00:00
         $date = mktime(12, 0, 0, $this->month, 1, $this->year);
         
         $first = (strftime("%w",$date) + 7 - $weekstarts) % 7;
+        // Get the name of the current month
         $monthName = utf8_strftime("%B",$date);
         
         $d = 1 - $first;
@@ -123,37 +113,35 @@ class Calendar
         $days_to_highlight = ($d + 7);
 
 		$loop1 = array();
-        while ($d <= $daysInMonth)
-        {
+        while ($d <= $daysInMonth) {
             $loop2 = array();
-            for ($i = 0; $i < 7; $i++)
-            {
+            for ($i = 0; $i < 7; $i++) {
             	$loop_array = array();		
-                if ($d > 0 && $d <= $daysInMonth)
-                {
+                if ($d > 0 && $d <= $daysInMonth) {
                 	$loop_array['empty'] = 'false';
                 	$loop_array['d'] = $d;
                 	$d_week = ($d - 7);
-
-                    if (preg_match("/day/i", basename($_SERVER['PHP_SELF'])))
-                    {
+                	
+                	// Get the filename of the initial called PHP-script to
+                	// identify which highlighting to use.
+                	$sCalledScript = basename($_SERVER['PHP_SELF']);
+                    if (preg_match("/day/i", $sCalledScript)) {
                         $loop_array['type'] = 'day';
-                    }
-                    elseif (preg_match("/week/i", basename($_SERVER['PHP_SELF'])))
-                    {
+                    } elseif (preg_match("/week/i", $sCalledScript)) {
 					    $loop_array['type'] = 'week';
-                        if (($this->day <= $d) && ($this->day > $d_week) && ($this->h))
+                        if (($this->day <= $d) && ($this->day > $d_week) && ($this->h)) {
                             $loop_array['high'] = 'true';
-                        elseif (($this->day < $days_to_highlight) && ($d < $days_to_highlight) && (($day - $daysInPrevMonth) > (-6)) && ($this->month == (($month + 1)%12)) && ($first != 0))
+                        } elseif (($this->day < $days_to_highlight) && ($d < $days_to_highlight) && (($day - $daysInPrevMonth) > (-6)) && ($this->month == (($month + 1)%12)) && ($first != 0)) {
                             $loop_array['high'] = 'true';
-                        else
+                        } else {
                             $loop_array['high'] = 'false';
-                    }
-                    elseif (preg_match("/month/i", basename($_SERVER['PHP_SELF'])))
+                        }
+                    } elseif (preg_match("/month/i", $sCalledScript))
                         $loop_array['type'] = 'month';
-                }
-                else
+                    }
+                } else {
                     $loop_array['empty'] = 'true';
+                }
                 
                 $d++;
                 $loop2[] = $loop_array;
@@ -161,31 +149,36 @@ class Calendar
             $loop1[] = $loop2;
         }
         
-        $smarty->assign('days_to_highlight',$days_to_highlight);
-        $smarty->assign('daysInPrevMonth',$daysInPrevMonth);
-        $smarty->assign('first',$first);
-        $smarty->assign('monthName',$monthName);
-        $smarty->assign('year',$this->year);
-        $smarty->assign('month',$this->month);
-        $smarty->assign('loop1',$loop1);
-        $smarty->assign('room',$this->room);
-        $smarty->assign('area',$this->area);
-        $smarty->assign('dweek',$d_week);
-        $smarty->assign('h',$this->h);
-        $smarty->assign('day',(int)$this->day);
-        $smarty->assign('dmy',$this->dmy);
+        // Assign all need variable for the template
+        $smarty->assign(array(
+        	'days_to_highlight' => $days_to_highlight,
+        	'daysInPrevMonth' => $daysInPrevMonth,
+        	'first' => $first,
+			'monthName' => $monthName,
+			'year' => $this->year,
+        	'month' => $this->month,
+        	'loop1' => $loop1,
+        	'room' => $this->room, 
+        	'area' => $this->area, 
+        	'dweek' => $d_week, 
+        	'h' => $this->h,
+        	'day' => intval($this->day),
+			'dmy' => $this->dmy
+        ));
         // First days
         $basetime = mktime(12,0,0,6,11+$weekstarts,2000);
-      	for ($i = 0, $firstdays = array(); $i < 7; $i++)
-      	{
+      	for ($i = 0, $firstdays = array(); $i < 7; $i++) {
         	$show = $basetime + ($i * 24 * 60 * 60);
          	$firstdays[] = utf8_strftime('%a',$show);
       	}
       	$smarty->assign('firstdays',$firstdays);
-      	// catch Output
+      	// At this point we have assigned all variable we need to disply the 
+      	// Smarty template for the minicals, but we do not want to send the 
+      	// calendars directly to the user, so we will capture the ouput with 
+      	// PHP's ouput handling functions.
       	ob_start();
         $smarty->display('minicals.tpl');
-        $s.= ob_get_contents();
+        $s = ob_get_contents();
         ob_end_clean();
         
         return $s;
@@ -193,7 +186,9 @@ class Calendar
 }
 
 /**
- * Specifies the class and builds the three calenders
+ * Build three calendars for a given month (calendars include the previous and
+ * the upcoming month). The calendars are embedded in table cells (td-tags).
+ * There is no surronding tr- or table-tag, this must be defined separately.
  * 
  * @author David Wilkinson <davidw@cascade.org.uk>, gwalker, Uwe L. Korn <uwelk@xhochy.org>
  * @param int $year
@@ -205,21 +200,27 @@ class Calendar
  */
 function minicals($year, $month, $day, $area, $room, $dmy) 
 {
+	// The time for the previous month, 1st, 12:00:00
 	$lastmonth = mktime(12, 0, 0, $month-1, 1, $year);
-	$thismonth = mktime(12, 0, 0, $month,   $day, $year);
+	// The time for the given month, 1st, 12:00:00
+	$thismonth = mktime(12, 0, 0, $month, $day, $year);
+	// The time for the upcoming month, 1st, 12:00:00
 	$nextmonth = mktime(12, 0, 0, $month+1, 1, $year);
 	
+	// Display the calendar for the previous month
     puts('<td style="width: 100%">&nbsp;</td>');
 	puts('<td class="calendar-wrapper">');
 	$cal = new Calendar(date("d",$lastmonth), date("m",$lastmonth), date("Y",$lastmonth), 0, $area, $room, $dmy);
 	echo $cal->getHTML();
 	echo "</td>";
 	
+	// Display the calendar for the current month
 	puts('<td class="calendar-wrapper">');
 	$cal = new Calendar(date("d",$thismonth), date("m",$thismonth), date("Y",$thismonth), 1, $area, $room, $dmy);
 	echo $cal->getHTML();
 	echo "</td>";
 	
+	// Display the calendar for the upcoming month
 	puts('<td class="calendar-wrapper">');
 	$cal = new Calendar(date("d",$nextmonth), date("m",$nextmonth), date("Y",$nextmonth), 0, $area, $room, $dmy);
 	echo $cal->getHTML();
