cd5b9321de2c85bb140ea370e3e8c4816de5a0b2
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-04-02 13:12:12 +0000

    Removed PHP_STRICT warings during Smarty plugin loading.

--- schoorbs-includes/smarty.functions.php
+++ schoorbs-includes/smarty.functions.php
@@ -30,5 +30,5 @@ $smarty->template_dir = dirname(__FILE__).'/../schoorbs-misc/templates/';
 $smarty->compile_dir = dirname(__FILE__).'/Smarty/templates_c/';
 $smarty->cache_dir = dirname(__FILE__).'/Smarty/cache/';
 $smarty->config_dir = dirname(__FILE__).'/Smarty/configs/';
-$smarty->register_function('get_vocab', smarty_function_get_vocab);
-$smarty->register_function('genDateSelector', smarty_function_genDateSelector);
+$smarty->register_function('get_vocab', 'smarty_function_get_vocab');
+$smarty->register_function('genDateSelector', 'smarty_function_genDateSelector');
