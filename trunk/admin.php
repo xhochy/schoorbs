92d6dfca663cf423ede843853430786f9d54f874
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-04-13 17:10:09 +0000

    All database stuff in admin.php will be done via ORM.

--- trunk/admin.php
+++ trunk/admin.php
@@ -7,7 +7,7 @@
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
  */
 
-## Includes ##
+/// Includes ///
 
 /** The Schoorbs configuration **/
 require_once 'config.inc.php';
@@ -15,27 +15,28 @@ require_once 'config.inc.php';
 require_once 'schoorbs-includes/global.web.php';
 /** The common functions used everywhere */
 require_once 'schoorbs-includes/global.functions.php';
-/** The database layer */
-require_once "schoorbs-includes/database/$dbsys.php";
 /** The authentication layer */
 require_once 'schoorbs-includes/authentication/schoorbs_auth.php';
 /** The modern ORM databse layer */
 require_once 'schoorbs-includes/database/schoorbsdb.class.php';
 
-## Var Init ##
+/// Var Init ///
 
 /** day, month, year **/
 list($day, $month, $year) = input_DayMonthYear();
 
 /** area **/
 $area = input_Area();
-// Get the name of the area we are working on out of the database
-$area_name = areaGetName($area);
 
-## Main ##
+/// Main ///
 
 // Only administrators should be able to administrate Schoorbs
 if(!getAuthorised(2)) showAccessDenied();
+
+// Get the area as an ORM instance
+$oArea = Area::getById($area);
+// Get the name of the area we are working on out of the database
+$area_name = $oArea->getName();
 
 // Print out the (X)HTML-header
 print_header();
@@ -52,24 +53,24 @@ if (count($aAreas) === 0) {
 }
 
 // Collect all rooms in the choosen area
-$aRooms = array();
-if(isset($area)) {
-	$res = sql_query("SELECT id, room_name, description, capacity FROM $tbl_room where area_id = "
-		.sql_escape_arg($area)." ORDER BY room_name");
-	if (!$res) fatal_error(0, sql_error());
-	if (sql_count($res) == 0) {
-		$norooms = 'true';
-	} else {
-		for ($i = 0; ($row = sql_row($res, $i)); $i++) {
-			$aRooms[] = array('id' => $row[0], 'name' => $row[1], 'description' => $row[2], 'capacity' => $row[3]);
-        }
-		$norooms = 'false';
-	}
+//
+// $bNoRooms will determinate in the template if we should show a list of rooms
+// or a remark that there are no rooms available at the moment.
+$aRooms = Room::getRooms($oArea);
+if (count($aRooms) === 0) {
+	$bNoRooms = 'true';
+} else {
+	$bNoRooms = 'false';
 }
 
 // Assign the variables for use in the template system
+/** 
+ * @todo In future the variables norooms and noareas should be removed, the
+ *       templates should get intelligent, so that could determinate on their 
+ *       own, if there have rooms/areas, which they could display. 
+ */
 $smarty->assign(array(
-    'norooms' => $norooms,
+    'norooms' => $bNoRooms,
     'rooms' => $aRooms,
     'areas' => $aAreas,
     'noareas' => $bNoAreas,
