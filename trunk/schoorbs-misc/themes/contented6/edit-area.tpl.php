aa4050f0cc9753abbefbe2c37137e6fb11ba96d6
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-08-09 19:33:21 +0000

    Added add-room

--- trunk/schoorbs-misc/themes/contented6/edit-area.tpl.php
+++ trunk/schoorbs-misc/themes/contented6/edit-area.tpl.php
@@ -3,7 +3,7 @@
   <br /><br />
   <div class="form">
     <form method="post" action="<?php echo self::makeInternalUrl('edit-area.php', array('area' => $area->getId())); ?>">
-      <label for="area-name"><?php echo get_vocab('name'); ?></label>: <input type="text" id="area-name" name="area-name" value="<?php echo ht($area->getName()); ?>" />
+      <label for="area-name"><?php echo get_vocab('name'); ?></label> <input type="text" id="area-name" name="area-name" value="<?php echo ht($area->getName()); ?>" />
       <br />
       <input type="submit"value="<?php echo get_vocab('change'); ?>" />
     </form>
