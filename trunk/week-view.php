ad6315a117c43ecba3c9065921f32b5b3d933d7f
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-08-15 21:55:53 +0000

    Week-view matrix is now working

--- trunk/week-view.php
+++ trunk/week-view.php
@@ -25,9 +25,12 @@ require_once 'schoorbs-includes/schoorbstpl.class.php';
 /** day, month, year */
 list($day, $month, $year) = input_DayMonthYear();
 
+/** Get the room we should display */
+$oRoom = Room::getById(input_Room());
+
 /// Main ///
 
-// Set the date back to the previous $wTODOeekstarts day (Sunday, if 0):
+// Set the date back to the previous $weekstarts day (Sunday, if 0):
 $time = mktime(12, 0, 0, $month, $day, $year);
 if (($weekday = (date('w', $time) - $weekstarts + 7) % 7) > 0) {
 	$time -= $weekday * 86400;
@@ -44,9 +47,60 @@ $nEndTime = mktime($eveningends, $eveningends_minutes, 0, $month,$day + 6,
 // Build up Week Matrix
 // --> Index: Time/Period name
 // --> contains an 7-sized array of the appointments of this slot
-	
+
+// How many entry units does a day offer?
+// (morning - evening) / resolution  // all in minutes
+$nUnitsPerDay  = ($eveningends * 60) + $eveningends_minutes;
+// To get the timespan, remove the morning
+$nUnitsPerDay -= ($morningstarts * 60) + $morningstarts_minutes;
+// Divide through the resolution to get the number of units
+$nUnitsPerDay /= $resolution / 60;
+// we do not want any floats here, so keep it as an int
+$nUnitsPerDay = intval($nUnitsPerDay);
+
+// save all entries in the following array structure:
+// $aEntry[0..6][Morning..Evening(in seconds))] = Entry
+// If there is no entry in a unit, we will set that array entry to -1, not to
+// null since null is equal to unset($aEntry[..][..]), but we want a full 
+// timetable matrix.
+$aEntry = array();
+$aUniqueEntry = array();
+
+// iterate through the days
+for ($nDay = 0; $nDay < 7; $nDay++) {
+	$aEntry[$nDay] = array();
+	// Iterate through the single uints
+	for ($nUnit = 0; $nUnit < $nUnitsPerDay; $nUnit++) {
+		$nTime = mktime($morningstarts, $morningstarts_minutes, 0, $month, $day + $nDay, $year);
+		$nTime+= $nUnit * $resolution;
+		
+		
+		/** @todo Only query once for the whole week **/
+		$aEntries = Entry::getBetween($oRoom, $nTime, $nTime + $resolution);
+		/**----error---todo: go on
+		entry suchen, id finden**/
+		
+		if ($enable_periods) {
+			$sTime = $periods[$nUnit];
+		} else {
+			$sTime = date('H:i', $nTime);
+		}
+
+		if (count($aEntries) > 0) {
+			//var_dump($aEntries);die();
+			//entry per id laden
+			$oEntry = $aEntries[0];
+			$aEntry[$nDay][$sTime] = $oEntry;
+			$aUniqueEntry[$oEntry->getStartTime()] = $oEntry;
+		} else {
+			$aEntry[$nDay][$sTime] = -1;
+		}
+	}
+}
 
 SchoorbsTPL::populateVar('nStartTime', $nStartTime);
 SchoorbsTPL::populateVar('nEndTime', $nEndTime);	
+SchoorbsTPL::populateVar('uniqueEntries', $aUniqueEntry);
+SchoorbsTPL::populateVar('entries', $aEntry);
 SchoorbsTPL::renderPage('week-view');
 
