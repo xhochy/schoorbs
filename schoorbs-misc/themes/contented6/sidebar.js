4f9b983fa9389b2a99cf4f300f946c6da2a884ad
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-09-10 20:57:51 +0000

    Added several comments to the Contented6 Javascript files

--- schoorbs-misc/themes/contented6/sidebar.js
+++ schoorbs-misc/themes/contented6/sidebar.js
@@ -1,26 +1,105 @@
-var page = '';
-var documentRoot = '';
-var area = null;
-var room = null;
-var year = null, month = null, day = null;
+/**
+ * Handle all things loacted in the sidebar
+ *
+ * @author Uwe L. Korn <uwelk@xhochy.org>
+ */
 
+/** 
+ * The filename of the loaded (X)HTML document
+ * 
+ * @type String
+ */
+var page = '';
+
+/**
+ * This is equal to the "directory name" of the file, just in HTTP
+ *
+ * @type String
+ */
+var documentRoot = '';
+
+/**
+ * The area in which we are at the moment
+ *
+ * @type String
+ * @todo In future this should be an Int
+ */
+var area = null;
+
+/**
+ * The room in which we are at the moment
+ *
+ * @type String
+ * @todo In future this should be an Int
+ */
+var room = null;
+
+/**
+ * The year in which we are displaying
+ *
+ * @type String
+ * @todo In future this should be an Int
+ */
+var year = null;
+
+/**
+ * The month in which we are displaying
+ *
+ * @type String
+ * @todo In future this should be an Int
+ */
+var month = null;
+
+/**
+ * The day in which we are displaying
+ *
+ * @type String
+ * @todo In future this should be an Int
+ */
+var day = null;
+
+/**
+ * Handle the selection of a date in the CalendarPicker widget
+ *
+ * @author Uwe L. Korn <uwelk@xhochy.org>
+ */
 function calendarPickerHandleSelect(type,args,obj) {
-	var dates = args[0]; 
-	var date = dates[0];
-	var cPyear = date[0], cPmonth = date[1], cPday = date[2];
-	var params = ['month=' + cPmonth.toString(), 'day=' + cPday.toString(), 'year=' + cPyear.toString()];
+	// Just get the selected date out of the arguments for this event
+	var date = args[0][0];
+	// Make up all URL-parameters for the selected date
+	var params = ['month=' + date[1].toString(), 'day=' + date[2].toString(), 'year=' + date[0].toString()];
+	// If an area was already set, so add it to the new URL
 	if (area != null) {
 		params.push('area=' + area);
 	}
+	// If an room was already set, so add it to the new URL
 	if (room != null) {
 		params.push('room=' + room);
 	}
+	// Build up the new URL and load the page
 	location.href = documentRoot + page + '.php?' + params.join('&');
 }
 
+/**
+ * Parse all needed parameters out of the url.
+ *
+ * The following parameters are parsed:
+ *   -> The filename
+ *   -> room
+ *   -> area
+ *   -> year
+ *   -> day
+ *   -> month
+ *
+ * @author Uwe L. Korn <uwelk@xhochy.org>
+ */
 function parseThingsOutOfUrl() {
-	var regexp = /([^/]+)\.php\?[^?]*$/i
+	// Find the document root, we specify it as all characters in front of
+	// a string which could contain everything expect a /. We assume that 
+	// the filename ends on php and may have additional URL parameters.
+	var regexp = /([^/]+)\.php(\?[^?]*)?$/i
 	documentRoot = location.href.replace(regexp, '');
+	// Get the page name, this is the filename *without* '.php'
 	page = regexp.exec(location.href)[1];
 	regexp = /[&?]room=([\d]+)/i
 	room = regexp.exec(location.href);
