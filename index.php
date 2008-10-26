05f3ece006a0afaa96714bf962cb002f7e0f730a
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-10-26 09:22:24 +0000

    Removed database code from index.php

--- index.php
+++ index.php
@@ -13,11 +13,10 @@
 ## Includes ##
 
 /** The Schoorbs configuration file */
-require_once "config.inc.php";
+require_once 'config.inc.php';
 /** The global include for webscripts */
 require_once 'schoorbs-includes/global.web.php';
-/** The database functions */
-require_once "schoorbs-includes/database/$dbsys.php";
+
 
 ## Vars ##
 
@@ -35,20 +34,6 @@ case "week":
     break;
 default:
     $redirect_str = "day-view.php?day=$day&month=$month&year=$year";
-}
-
-if(!empty($default_room)) {
-    $sQuery = 'SELECT area_id FROM $tbl_room WHERE id = '
-        .sql_escape_arg($default_room);
-    $res = sql_query($sql);
-    if ($res) {
-        if (sql_count($res) == 1) {
-            $row = sql_row($res, 0);
-            $area = $row[0];
-            $room = $default_room;
-            $redirect_str .= "&area=$area&room=$room";
-        }
-    }
 }
 
 header("Location: $redirect_str");
