6b36a7acbd69e3cfdc7d8f9f1490e8f8a21e8b0d
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-11-04 15:05:47 +0000

    Added some comments to edit-area

--- trunk/edit-area.php
+++ trunk/edit-area.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * Edit an area
+ * Edit an area. Mainly the only thing which could be edited is the name of this
+ * area.
  * 
  * @author Uwe L. Korn <uwelk@xhochy.org>
  * @package Schoorbs
@@ -21,23 +22,32 @@ if (!getAuthorised(2)) {
 	showAccessDenied();
 }
 
+// Check if an area id was supplied, if there is none, we won't know which area
+// we want to edit, a default area selecton might be nice, but if the user 
+// doesn't directly knows that he is editing a random area, he might change the
+// false area.
 if (!isset($_REQUEST['area'])) {
 	SchoorbsTPL::error(Lang::_('No valid area(-id) was provided!'));
 	exit();
+// An emty area id might indicated a false call, if we would use intval() on 
+// this, it will return 0, so area 0 would be edited which might be confusing.
 } else if (empty($_REQUEST['area'])) {
 	SchoorbsTPL::error(Lang::_('No valid area(-id) was provided!'));
 	exit();
 }
 
+// Get the area id as an integer.
 $nArea = intval($_REQUEST['area']);
 
 /** @todo report if an empty string was submitted **/
 if (isset($_REQUEST['area-name']) && !empty($_REQUEST['area-name'])) {
-	// Update! and redirect
+	// Get the area out of the database
 	$oArea = Area::getById($nArea);
+	// Update its name.
 	$oArea->setName(unslashes($_REQUEST['area-name']));
-	// destruct to save it
+	// Destruct to save it
 	$oArea = null;
+	// redirect to the administration view of this area.
 	header(sprintf('Location: administration.php?area=%d', $nArea));
 } else {
 	// Edit!
