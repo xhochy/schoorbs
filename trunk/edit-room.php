95200c9fb0fb886153344722aeeac1418be6b971
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-11-04 15:13:49 +0000

    Added some comments to edit-room

--- trunk/edit-room.php
+++ trunk/edit-room.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Edit a room
+ * Edit a room, the name, the capacity and its description can be edited.
  * 
  * @author Uwe L. Korn <uwelk@xhochy.org>
  * @package Schoorbs
@@ -15,42 +15,41 @@ require_once 'config.inc.php';
 require_once 'schoorbs-includes/global.web.php';
 /** The general functions */ 
 require_once 'schoorbs-includes/global.functions.php';
-/** The modern ORM databse layer */
-require_once 'schoorbs-includes/database/schoorbsdb.class.php';
-/** The authetication wrappers */
-require_once 'schoorbs-includes/authentication/schoorbs_auth.php';
-/** The template system */
-require_once 'schoorbs-includes/schoorbstpl.class.php';
+
+/// Main ///
 
 // Only administrators should be able to create rooms and areas
 if (!getAuthorised(2)) {
 	showAccessDenied();
 }
 
+// Check if an room id was supplied, if there is none, we won't know which room
+// we want to edit, a default room selecton might be nice, but if the user 
+// doesn't directly knows that he is editing a random room, he might change the
+// false room.
 if (!isset($_REQUEST['room'])) {
 	SchoorbsTPL::error(Lang::_('No valid room(-id) was provided!'));
 	exit();
-}
-
-if (empty($_REQUEST['room'])) {
+// An emty room id might indicate a false call, if we would use intval() on 
+// this, it will return 0, so room 0 would be edited which might be confusing.
+} else if (empty($_REQUEST['room'])) {
 	SchoorbsTPL::error(Lang::_('No valid room(-id) was provided!'));
 	exit();
 }
 
+// Get the room id as an integer.
 $nRoom = intval($_REQUEST['room']);
-
-if ($nRoom != $_REQUEST['room']) {
-	SchoorbsTPL::error(Lang::_('No valid room(-id) was provided!'));
-	exit();
-}
 
 /** @todo report if an empty string was submitted **/
 if (isset($_REQUEST['room-name']) && !empty($_REQUEST['room-name'])) {
-	// Update! and redirect
+	// Get the room out of the database
 	$oRoom = Room::getById($nRoom);
+	// Set the new values
 	$oRoom->setName(unslashes($_REQUEST['room-name']));
 	$oRoom->setCapacity(intval($_REQUEST['capacity']));
 	$oRoom->setDescription(unslashes($_REQUEST['description']));
+	// Get the id of the area of this room to know where we will redirect
+	// the user after he finished editing the room.
 	$nArea = $oRoom->getArea()->getId();
 	// destruct to save it
 	$oRoom = null;
