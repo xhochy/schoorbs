c542be76222295cbcb74f3fd3e4dc6f58b5a1f06
xhochy <xhochy@cbedd411-1c29-0410-850b-bbd0d93afdff>
2008-11-04 15:17:34 +0000

    Added some comments to the help page

--- help.php
+++ help.php
@@ -1,8 +1,13 @@
 <?php
 /**
- * The help-page
+ * Display the help page. 
+ *
+ * The help textes reside in the translation file, so that they are translate-
+ * able through Launchpad too. In contrast to Schoorbs 1.x/MRBS 1.x the user is
+ * presented the FAQ's which fit best to his selected language in the browser
+ * and not the language which was choosen in the configuration file.
  * 
- * @author thierry_bo, Uwe L. Korn <uwelk@xhochy.org>
+ * @author Uwe L. Korn <uwelk@xhochy.org>
  * @package Schoorbs
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
  */
@@ -15,18 +20,16 @@ require_once 'schoorbs-includes/global.web.php';
 require_once 'schoorbs-includes/global.functions.php';
 /** The versio information of Schoorbs */
 require_once 'schoorbs-includes/version.php';
-/** The modern ORM databse layer */
-require_once 'schoorbs-includes/database/schoorbsdb.class.php';
-/** The template system */
-require_once 'schoorbs-includes/schoorbstpl.class.php';
 
 // Obfuscate Admin-E-Mail
 $sAdminMail = '';
+// Go through each character
 for ($i = 0; $i < strlen($mrbs_admin_email); $i++) {
 	// Use hex character representation since its shorter than decimal
 	$sAdminMail .= '&#x'. sprintf("%x",ord($mrbs_admin_email[$i])).';';
 }
 
+// Populate all data to the theming engine and display the help template
 SchoorbsTPL::populateVar('admin', $mrbs_admin);
 SchoorbsTPL::populateVar('adminMail', $sAdminMail);
 SchoorbsTPL::populateVar('version', get_schoorbs_version());
